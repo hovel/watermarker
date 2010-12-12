7e54a4512bd84e5fceb41e4496fa00ededc74418
Pavel Zhukov <gelios@gmail.com>
2010-12-13 01:51:26 +0300

    New library version Sorl-thumbnail integration

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+        name='watermarker',
+        version='1.0',
+        packages=['watermarker'],
+        install_requires=[
+            'pil',
+        ],
+        author='Pavel Zhukov',
+        author_email='gelios@gmail.com',
+        description='Library for add text watermarks to images using PIL',
+        long_description = open('README').read(),
+        license='GPL',
+        keywords='watermark, library',
+        url='http://bitbucket.org/zeus/watermarker'
+        )
+
